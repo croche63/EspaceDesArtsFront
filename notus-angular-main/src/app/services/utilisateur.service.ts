956885c5c0bc385b037c8ae162671f5b9f14399b
croche63 <111734808+croche63@users.noreply.github.com>
2022-09-07 16:53:30 +0200

    Pages login/register semi fonctionnelles

--- notus-angular-main/src/app/services/utilisateur.service.ts
+++ notus-angular-main/src/app/services/utilisateur.service.ts
@@ -6,8 +6,8 @@ import { Utilisateur } from '../models/utilisateur';
 @Injectable({
   providedIn: 'root'
 })
-export class ArtisteService {
-  private baseUrl = "http://localhost:7070/Utilisateurs";
+export class UtilisateurService {
+  private baseUrl = "http://localhost:7070/utilisateurs";
 
   constructor(private httpClient:HttpClient) { }
 
@@ -15,15 +15,15 @@ export class ArtisteService {
     return this.httpClient.get(this.baseUrl);
   }
 
-  public save(image:File,utilisateur:Utilisateur):Observable<any>{
+  public save(/* image:File, */utilisateur:Utilisateur):Observable<any>{
     const formData=new FormData();
-    formData.append('nomU',utilisateur.nom);
+    formData.append('nom',utilisateur.nom);
     formData.append('prenom',utilisateur.prenom);
     formData.append('username',utilisateur.username);
     formData.append('password',utilisateur.password);
     formData.append('email',utilisateur.email);
     formData.append('numeroTel',utilisateur.numeroTel);
-    formData.append('fileU',image);
+/*     formData.append('fileU',image); */
     const requete = new HttpRequest('POST',this.baseUrl,formData,
     {reportProgress:true,responseType:'text'});
     return this.httpClient.request(requete);
--- notus-angular-main/src/app/views/auth/login/login.component.ts
+++ notus-angular-main/src/app/views/auth/login/login.component.ts
@@ -1,11 +1,21 @@
+import { HttpClient } from "@angular/common/http";
 import { Component, OnInit } from "@angular/core";
+import { Router } from "@angular/router";
+import { AppService } from "src/app/app.service";
 
 @Component({
   selector: "app-login",
   templateUrl: "./login.component.html",
 })
-export class LoginComponent implements OnInit {
-  constructor() {}
+export class LoginComponent {
 
-  ngOnInit(): void {}
+  credentials = {username: '', password:''}
+
+  constructor(private appService:AppService, private httpClient:HttpClient, private router:Router) { }
+
+  // TODO rediriger vers autre page
+  login() {
+    this.appService.authenticate(this.credentials, () => {this.router.navigateByUrl("/user-profile")});
+    return false;
+  }
 }
