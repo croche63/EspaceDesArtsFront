e41fc358f170588c9580216d547a9c5bf62b54d7
ClaudeVeillas <claude.veillas@gmail.com>
2022-09-07 15:04:15 +0200

    model avec liens Claude

--- notus-angular-main/src/app/models/salle-exposition.ts
+++ notus-angular-main/src/app/models/salle-exposition.ts
@@ -1,8 +1,24 @@
 import { Byte } from "@angular/compiler/src/util";
+import { Adresse } from "./adresse";
+import { CommentaireSalleExposition } from "./commentaire-salle-exposition";
+import { EvaluationArtiste } from "./evaluation-artiste";
+import { Oeuvre } from "./oeuvre";
+import { Proprietaire } from "./proprietaire";
+import { Reservation } from "./reservation";
+import { SignalementSalleExposition } from "./signalement-salle-exposition";
 
 export class SalleExposition {
     id!:number;
     libelle!:string;
     logo!:Byte;
     dimensionSalle!:string;
+    adresse!:Adresse;
+    proprietaire!:Proprietaire;
+    signalementSalleExposition!:SignalementSalleExposition;
+    reservation!:Reservation;
+    evaluationArtiste!:EvaluationArtiste;
+    commentaireSalleExposition!:CommentaireSalleExposition;
+    oeuvre!:Oeuvre;
+
+
 }
--- notus-angular-main/src/app/models/salle-virtuelle.ts
+++ notus-angular-main/src/app/models/salle-virtuelle.ts
@@ -1,6 +1,15 @@
+import { CommentaireSalleVirtuelle } from "./commentaire-salle-virtuelle";
+import { Oeuvre } from "./oeuvre";
+import { Proprietaire } from "./proprietaire";
+import { SignalementSalleVirtuelle } from "./signalement-salle-virtuelle";
+
 export class SalleVirtuelle {
     id!:number;
     libelle!:string;
     description!:string;
-    
+    signalementSalleVirtuelle!:SignalementSalleVirtuelle;
+    commentaireSalleVirtuelle!:CommentaireSalleVirtuelle;
+    proprietaire!:Proprietaire;
+    oeuvre!:Oeuvre;
+
 }
