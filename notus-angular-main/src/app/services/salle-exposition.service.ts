86fe8a571badf7f29d043edc65053d229e5a9a78
ClaudeVeillas <claude.veillas@gmail.com>
2022-09-07 14:12:50 +0200

    ajout service des salles Claude

---
+++ notus-angular-main/src/app/services/salle-exposition.service.ts
@@ -0,0 +1,26 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class SalleExpositionService {
+
+  private baseUrl = "http://localhost:3306/SalleExpositions";
+
+  constructor(private httpClient:HttpClient) { }
+
+  public findAll() : Observable<any>{
+    return this.httpClient.get(this.baseUrl);
+  }
+
+  public save(evaluationArtiste:any) : Observable<any>{
+    return this.httpClient.post(this.baseUrl,evaluationArtiste);
+  }
+
+  public delete(id:number) : Observable<any>{
+    return this.httpClient.delete(this.baseUrl+"/"+id);
+  }
+
+}
