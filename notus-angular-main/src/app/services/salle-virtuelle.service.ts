d86f329605a7f28a6044ecfbdffd4e67e36bc2dc
SaraSousa26 <sarafilipa26@gmail.com>
2022-09-09 11:29:33 +0200

    localhost change

--- notus-angular-main/src/app/services/salle-virtuelle.service.ts
+++ notus-angular-main/src/app/services/salle-virtuelle.service.ts
@@ -6,7 +6,7 @@ import { Observable } from 'rxjs';
   providedIn: 'root'
 })
 export class SalleVirtuelleService {
-  private baseUrl = "http://localhost:3306/SalleVirtuelles";
+  private baseUrl = "http://localhost:7070/SalleVirtuelles";
 
   constructor(private httpClient:HttpClient) { }
 
