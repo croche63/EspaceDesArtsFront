57c0e46881be2ee6530d3dfff393f43cbcacb108
GuillaumeSignoret <guillaume.signoret@outlook.com>
2022-09-10 00:40:21 +0200

    bidouillage

--- notus-angular-main/src/app/views/index/index.component.ts
+++ notus-angular-main/src/app/views/index/index.component.ts
@@ -1,6 +1,12 @@
 import { Component, OnInit } from "@angular/core";
 import { Router } from "@angular/router";
+import { AppService } from "src/app/app.service";
+import { Adresse } from "src/app/models/adresse";
+import { Reservation } from "src/app/models/reservation";
+import { SalleExposition } from "src/app/models/salle-exposition";
 import { SalleVirtuelle } from "src/app/models/salle-virtuelle";
+import { OeuvreService } from "src/app/services/oeuvre.service";
+import { SalleExpositionService } from "src/app/services/salle-exposition.service";
 import { SalleVirtuelleService } from "src/app/services/salle-virtuelle.service";
 
 @Component({
@@ -9,9 +15,24 @@ import { SalleVirtuelleService } from "src/app/services/salle-virtuelle.service"
 })
 export class IndexComponent implements OnInit {
   constructor(private salleVirtuelleService:SalleVirtuelleService,
-    private router:Router) {}
+    private oeuvreService:OeuvreService,
+    private salleExpositionService:SalleExpositionService,
+    private appService:AppService,private router:Router) {}
+    salleExpositions!: SalleExposition[];
+    adresse!:Adresse;reservations!:Reservation[];
+    nbr!:number
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    this.findAllSallesExposition();
+    // nbr:number = this.reservations.length;
+
+  }
+
+  findAllSallesExposition(){
+    this.salleExpositionService.findAll().subscribe((data: any[]) => {this.salleExpositions = data;
+    console.log(this.salleExpositions);
+  });
+  }
 
 
   selectSalleVirtu(salleV:SalleVirtuelle){
@@ -21,7 +42,14 @@ export class IndexComponent implements OnInit {
     localStorage.setItem("afficheSalleV",salleV.id.toString())
     // step 3
     this.router.navigate(["/profile",salleV.id])
-
-
   }
+  selectSalleExpo(salleE:SalleExposition){
+    //step 2 
+    localStorage.removeItem("afficheSalleE")
+    // step 1
+    localStorage.setItem("afficheSalleE",salleE.id.toString())
+    // step 3
+    this.router.navigate(["/profile",salleE.id])
+  }
+
 }
