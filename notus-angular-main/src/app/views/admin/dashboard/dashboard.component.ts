25a1e44b3269abdb17cd85f85710d586f2c2f8b4
croche63 <111734808+croche63@users.noreply.github.com>
2022-09-09 16:05:28 +0200

    Gestion admin tableau affichage

--- notus-angular-main/src/app/views/admin/dashboard/dashboard.component.ts
+++ notus-angular-main/src/app/views/admin/dashboard/dashboard.component.ts
@@ -1,4 +1,10 @@
 import { Component, OnInit } from "@angular/core";
+import { Utilisateur } from "src/app/models/utilisateur";
+import { RoleService } from "src/app/services/role.service";
+import { SignalementOeuvreService } from "src/app/services/signalement-oeuvre.service";
+import { SignalementSalleExpositionService } from "src/app/services/signalement-salle-exposition.service";
+import { SignalementSalleVirtuelleService } from "src/app/services/signalement-salle-virtuelle.service";
+import { UtilisateurService } from "src/app/services/utilisateur.service";
 
 @Component({
   selector: "app-dashboard",
@@ -7,12 +13,33 @@ import { Component, OnInit } from "@angular/core";
 export class DashboardComponent implements OnInit {
 
   //TODO Gestion des abonnés des espaces d'exposition
-  // Affichage des utilisateurs
+  // Affichage des utilisateurs et de leurs rôles
   // Possibilité de leur associer un role
 
-  //TODO Gestion des reclamations (afficher les reclamations)
+  //TODO Gestion des reclamations
+  // Supprimer reclamations ou oeuvre
 
-  constructor() {}
+  utilisateurs:any[];
+  roles:any[];
 
-  ngOnInit() {}
+  signalementOeuvre:any[];
+  signalementSalleExposition:any[];
+  signalementSalleVirtuelle:any[];
+
+  constructor(
+    private utilisateurService:UtilisateurService, private roleService:RoleService, private signalementOeuvreService:SignalementOeuvreService,
+    private signalementSalleExpositionService:SignalementSalleExpositionService, private signalementSalleVirtuelleService:SignalementSalleVirtuelleService
+    ) {}
+
+  ngOnInit() {
+    this.findAllInfos();
+  }
+
+  findAllInfos() {
+    this.utilisateurService.findAll().subscribe((data: any[]) => {this.utilisateurs = data;});
+    this.roleService.findAll().subscribe((data: any[]) => {this.roles = data;});
+    this.signalementOeuvreService.findAll().subscribe((data: any[]) => {this.signalementOeuvre = data;});
+    this.signalementSalleExpositionService.findAll().subscribe((data: any[]) => {this.signalementSalleExposition = data;});
+    this.signalementSalleVirtuelleService.findAll().subscribe((data: any[]) => {this.signalementSalleVirtuelle = data;});
+  }
 }
