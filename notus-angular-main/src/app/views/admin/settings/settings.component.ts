1f21bf17f75c962b7ab82a9531c6773dbac55dcf
croche63 <111734808+croche63@users.noreply.github.com>
2022-09-13 11:14:57 +0200

    Page proprio progres

--- notus-angular-main/src/app/views/admin/settings/settings.component.ts
+++ notus-angular-main/src/app/views/admin/settings/settings.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from "@angular/core";
+import { AppService } from "src/app/app.service";
+import { Proprietaire } from "src/app/models/proprietaire";
+import { ProprietaireService } from "src/app/services/proprietaire.service";
 
 @Component({
   selector: "app-settings",
@@ -11,7 +14,30 @@ export class SettingsComponent implements OnInit {
   //TODO Planification evenements (Ajout, modification, supression evenements)
   //TODO Offrir espace virtuel
 
-  constructor() {}
+  proprietaire:Proprietaire = new Proprietaire()
+  username:string = this.appService.username;
 
-  ngOnInit(): void {}
+  constructor(
+    private appService:AppService,
+    private proprietaireService:ProprietaireService
+  ) {}
+
+  ngOnInit(): void {
+    this.findAllInfos();
+  }
+
+  findAllInfos() {
+    this.proprietaireService.findByUsername(this.username).subscribe((data: Proprietaire) => {this.proprietaire = data;console.log(this.proprietaire)});
+  }
+  
+  //Modal pour les oeuvres
+  displayStyle = "none";
+
+  openPopup() {
+    this.displayStyle = "block";
+  }
+
+  closePopupOeuvre() {
+    this.displayStyle = "none";
+  }
 }
