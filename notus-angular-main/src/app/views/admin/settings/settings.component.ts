b058f2eb1e371cbeddb846b40441f7a8a2b3c575
croche63 <111734808+croche63@users.noreply.github.com>
2022-09-13 16:33:12 +0200

    Page gestion proprietaire plus de fonctions

--- notus-angular-main/src/app/views/admin/settings/settings.component.ts
+++ notus-angular-main/src/app/views/admin/settings/settings.component.ts
@@ -1,7 +1,9 @@
 import { Component, OnInit } from "@angular/core";
 import { AppService } from "src/app/app.service";
 import { Proprietaire } from "src/app/models/proprietaire";
+import { Reservation } from "src/app/models/reservation";
 import { ProprietaireService } from "src/app/services/proprietaire.service";
+import { ReservationService } from "src/app/services/reservation.service";
 
 @Component({
   selector: "app-settings",
@@ -9,24 +11,29 @@ import { ProprietaireService } from "src/app/services/proprietaire.service";
 })
 export class SettingsComponent implements OnInit {
 
-  //TODO Reservation espace expo (voir les reservations de son espace)
-  //TODO Gestion espace expo (Modifier attributs)
-  //TODO Planification evenements (Ajout, modification, supression evenements)
+  //TODO Reservation espace expo (voir les reservations de son espace) X
+  //TODO Gestion espace expo (Afficher X, Modifier)
+  //TODO Planification evenements (Visualiser X, Ajout, modification, supression evenements)
   //TODO Offrir espace virtuel
 
-  proprietaire:Proprietaire = new Proprietaire()
+  reservations:any[];
+
+  proprietaire:Proprietaire = new Proprietaire();
   username:string = this.appService.username;
 
   constructor(
     private appService:AppService,
-    private proprietaireService:ProprietaireService
+    private proprietaireService:ProprietaireService,
+    private reservationService:ReservationService,
   ) {}
 
   ngOnInit(): void {
     this.findAllInfos();
+    console.log(this.proprietaire)
   }
 
   findAllInfos() {
+    this.reservationService.findAll().subscribe((data: any[]) => {this.reservations = data;});
     this.proprietaireService.findByUsername(this.username).subscribe((data: Proprietaire) => {this.proprietaire = data;console.log(this.proprietaire)});
   }
 
@@ -36,16 +43,5 @@ export class SettingsComponent implements OnInit {
     } else {
       return true;
     }
-  }
-  
-  //Modal pour les oeuvres
-  displayStyle = "none";
-
-  openPopup() {
-    this.displayStyle = "block";
-  }
-
-  closePopupOeuvre() {
-    this.displayStyle = "none";
   }
 }
