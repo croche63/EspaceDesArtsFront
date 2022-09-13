1f3cd380b6a6e31fdfcad7b238e1a5b1b737a4bb
SaraSousa26 <sarafilipa26@gmail.com>
2022-09-13 23:04:37 +0200

    modifications gestion proprietaire

--- notus-angular-main/src/app/views/admin/settings/settings.component.ts
+++ notus-angular-main/src/app/views/admin/settings/settings.component.ts
@@ -1,9 +1,13 @@
 import { Component, OnInit } from "@angular/core";
+import { FormBuilder, FormGroup, Validators } from "@angular/forms";
 import { AppService } from "src/app/app.service";
 import { Proprietaire } from "src/app/models/proprietaire";
 import { Reservation } from "src/app/models/reservation";
+import { SalleExposition } from "src/app/models/salle-exposition";
+import { ArtisteService } from "src/app/services/artiste.service";
 import { ProprietaireService } from "src/app/services/proprietaire.service";
 import { ReservationService } from "src/app/services/reservation.service";
+import { SalleExpositionService } from "src/app/services/salle-exposition.service";
 
 @Component({
   selector: "app-settings",
@@ -16,25 +20,38 @@ export class SettingsComponent implements OnInit {
   //TODO Planification evenements (Visualiser X, Ajout, modification, supression evenements)
   //TODO Offrir espace virtuel
 
+  editForm!: FormGroup;
+
   reservations:any[];
+
+  artistes:any[];
 
   proprietaire:Proprietaire = new Proprietaire();
   username:string = this.appService.username;
+
+  salleExposition:SalleExposition = new SalleExposition();
 
   constructor(
     private appService:AppService,
     private proprietaireService:ProprietaireService,
     private reservationService:ReservationService,
+    private salleExpositionService:SalleExpositionService,
+    private artisteService:ArtisteService,
   ) {}
 
   ngOnInit(): void {
     this.findAllInfos();
-    console.log(this.proprietaire)
+    console.log(this.proprietaire.prenom);
+    this.findAllArtistes();
   }
 
   findAllInfos() {
     this.reservationService.findAll().subscribe((data: any[]) => {this.reservations = data;});
     this.proprietaireService.findByUsername(this.username).subscribe((data: Proprietaire) => {this.proprietaire = data;console.log(this.proprietaire)});
+  }
+
+  findAllArtistes(){
+    this.artisteService.findAll().subscribe((data: any[]) => {this.artistes = data;});
   }
 
   isProprietaire(){
@@ -44,4 +61,20 @@ export class SettingsComponent implements OnInit {
       return true;
     }
   }
+
+  //Ajout d'une salle virtuelle
+  displayStyle1 = "none";
+  
+  openPopupSalle(){
+    this.displayStyle1 = "block";
+  }
+
+  closePopupSalle() {
+    this.displayStyle1 = "none";
+  }
+
+  saveSalle(){
+
+  }
+
 }
