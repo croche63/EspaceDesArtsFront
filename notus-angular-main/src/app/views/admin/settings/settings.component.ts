bb7f3d5797e21e9e84a82b54f4b300211d7fc3de
croche63 <111734808+croche63@users.noreply.github.com>
2022-09-13 10:00:49 +0200

    Preparation page proprietaire

--- notus-angular-main/src/app/views/admin/settings/settings.component.ts
+++ notus-angular-main/src/app/views/admin/settings/settings.component.ts
@@ -5,6 +5,12 @@ import { Component, OnInit } from "@angular/core";
   templateUrl: "./settings.component.html",
 })
 export class SettingsComponent implements OnInit {
+
+  //TODO Reservation espace expo (voir les reservations de son espace)
+  //TODO Gestion espace expo (Modifier attributs)
+  //TODO Planification evenements (Ajout, modification, supression evenements)
+  //TODO Offrir espace virtuel
+
   constructor() {}
 
   ngOnInit(): void {}
